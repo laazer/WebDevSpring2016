90946e097429d02b6c0cdacf5b7f242e6d376a60
laazer <brandt.j@husky.neu.edu>
2016-02-14 17:57:15 -0500

    added main controller file

---
+++ public/assignment/views/main/main.controller.js
@@ -0,0 +1,12 @@
+(function()
+{
+	angular
+		.module("FormBuilderApp")
+		.controller("MainController", MainController);
+		
+    function MainController ($scope, $location) {
+        $scope.$location = $location;
+        $scope.isLoggedIn = false;
+        $scope.isAdmin = false;
+    }
+})();
