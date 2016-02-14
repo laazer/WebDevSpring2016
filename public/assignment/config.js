ee050ab6db194f0e00ced9004489bcd71373d5a3
laazer <brandt.j@husky.neu.edu>
2016-02-14 17:56:56 -0500

    header now works as excpected

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -10,5 +10,25 @@
 					templateUrl: "views/home/home.view.html",
 					controller: "HomeController"
 				})
+				.when("/register", 
+				{
+					templateUrl: "views/users/register.view.html",
+					controller: "RegisterContoller"
+				})
+                .when("/login", 
+				{
+					templateUrl: "views/users/login.view.html",
+					controller: "LoginController"
+				})
+                .when("/profile", 
+				{
+					templateUrl: "views/users/profile.view.html",
+					controller: "ProfileController"
+				})
+                .when("/admin", 
+				{
+					templateUrl: "views/admin/admmin.view.html",
+					controller: "AdminController"
+				})
 		});
 })();
