635af6f74a0ea751c6510e0e86224857c636cddd
laazer <brandt.j@husky.neu.edu>
2016-02-26 18:05:22 -0500

    added forms service file

---
+++ public/assignment/services/forms.service.client.js
@@ -0,0 +1,67 @@
+(function()
+{
+	angular
+		.module("FormBuilderApp")
+		.factory("FormService", FormService);
+		
+    function FormService () {
+        var model = {
+            forms : [
+                    {"_id": "000", "title": "Contacts", "userId": 123},
+                    {"_id": "010", "title": "ToDo",     "userId": 123},
+                    {"_id": "020", "title": "CDs",      "userId": 234},
+             ],
+
+            createFormForUser: createFormForUser,
+            findAllFormsForUser: findAllFormsForUser,
+            deleteFormById: deleteFormById,
+            updateFormById: updateFormById
+            
+        };
+        return model;
+        
+        function createFormForUser(userId, form, callback) {
+            var lform = form;
+            lform.userId = userId;
+            lform.title = form.title;
+            lform._id = (new Date).getTime();
+            model.forms.push(lform);
+            callback(lform);
+        }
+       
+       function findAllFormsForUser(userId, callback) {
+           var result = model.forms.filter(function(value) {
+                return value.userId == userId;    
+           });
+           callback(result);
+       }
+       
+       function deleteFormById(formId, callback) {
+            for(var f in model.forms) {
+                if(model.forms[f]._id == formId) {
+                    model.forms.splice(f, 1);
+                    break;
+                }
+            }
+            callback(model.forms);    
+       }
+       
+       function updateFormById(formId, newForm, callback) {
+           var form = null;
+           for(var f in model.forms) {
+                if(model.forms[f]._id == formId) {
+                    cuser = model.forms[f];
+                    model.forms[f] = {
+                        "_id" : formId,
+                        "title" : newForm.title ? newForm.title : form.title,
+                        "userId" : newForm.userId ? newForm.userId : form.userId,
+                    }
+                    form = model.forms[f];
+                    break;
+                }
+            }
+            callback(form);   
+        }
+    }
+    
+})();
