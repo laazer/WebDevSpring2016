10f0d7bd08bfaa11d129462572fd0ee3da321672
laazer <brandt.j@husky.neu.edu>
2016-03-17 21:45:32 -0400

    fixed form service cp paste error

--- public/assignment/client/js/main.js
+++ public/assignment/client/js/main.js
@@ -1,3 +1,15 @@
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
+
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
--- public/assignment/client/services/forms.service.client.js
+++ public/assignment/client/services/forms.service.client.js
@@ -6,12 +6,6 @@
 		
     function FormService () {
         var model = {
-            forms : [
-                    {"_id": "000", "title": "Contacts", "userId": 123},
-                    {"_id": "010", "title": "ToDo",     "userId": 123},
-                    {"_id": "020", "title": "CDs",      "userId": 234},
-             ],
-
             createFormForUser: createFormForUser,
             findAllFormsForUser: findAllFormsForUser,
             deleteFormById: deleteFormById,
