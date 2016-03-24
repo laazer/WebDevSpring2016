92fa895c20af38933ffbf3e687af737af6a1d7a7
laazer <brandt.j@husky.neu.edu>
2016-03-24 16:42:07 -0400

    added package.json

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-var express = require('express');
-var uuid = require('node-uuid');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var app = express();
+var express       = require('express');
+var app           = express();
+var bodyParser    = require('body-parser');
+var multer        = require('multer');
+var passport      = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+var mongoose      = require('mongoose');
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-// Routes
-require('./public/assignment/server/app.js')(app);
 
 // Configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(multer);
+app.use(multer());
+app.use(session({ secret: process.env.PASSPORT_SECRET }));
+app.use(cookieParser())
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(express.static(__dirname + '/public'));
+app.use('/assignment', express.static(__dirname + '/public/assignment/client'));
+app.use('/project', express.static(__dirname + '/public/project/client'));
+
+// Routes
+require('./public/assignment/server/app.js')(app);
 
 // app.get('/hello', function(req, res){
 //     res.send('hello world');
 // });
 
-app.listen(port, ipaddress);
+app.listen(port, ipaddress, function() {
+  console.log("Starting server at: " + ipaddress + " on port " + port);
+});
