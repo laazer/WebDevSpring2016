a85af7f5fede3bde852eafef65fb212f5a6e095f
laazer <jabrandt31@gmail.com>
2016-09-15 15:14:44 -0400

    1.1 is done!

--- public/landing/views/main/main.controller.js
+++ public/landing/views/main/main.controller.js
@@ -15,15 +15,21 @@
 					github: "https://github.com/laazer",
 					linkedin: "https://www.linkedin.com/in/brandtjacob",
 					resume: "./assets/resume.pdf",
+					aboutMe1: "I'm an evolved form of code monkey. I squash bugs, talk to rubber ducks, and pray that the coffee maker is still working. I build websites, Mobile Apps, and the occasional video game.",
+					aboutMe2: "I roam the streets fighting crime as my alter ego, That Guy. I also can be found at the local gym, picking things up and then putting them down. I also might be in front of a stove top cooking dinner or at my computer watching Netflix and playing video games.",
 				};
 				$rootScope.me.getRandomImg = function() {
 					var img = $rootScope.me.img;
-					var result = img[Math.floor(Math.random() * (img.length - 1))];
+					var result = img[getRandomInt(0, img.length - 1)];
 					return result;
 				};
 				function range(n) {
 						// if(!n) return [];
 						return Array.apply(null, Array(n)).map(function (_, i) {return i;});
+				}
+
+				function getRandomInt(min, max) {
+					return Math.floor(Math.random() * (max - min + 1)) + min;
 				}
 
     }
